42ee3ea2c513e5268f9ee78d903e7096aa90f3a6
Georges-B. Michel <georges@reversense.com>
2022-01-24 01:21:48 +0100

    add more arg type

--- src/common/DataLabels.ts
+++ src/common/DataLabels.ts
@@ -3,10 +3,17 @@ export enum L {
     PATH,
     SIZE,
     FD,
+    DFD,
     FLAG,
     ATTRMODE,
     O_FLAGS,
     VADDR,
     MPROT,
-    OUTPUT_BUFFER
+    OUTPUT_BUFFER,
+    PID,
+    ERR,
+    SIG,
+    XATTR_LIST,
+    F_,
+    MFD // mapped fd
 }
