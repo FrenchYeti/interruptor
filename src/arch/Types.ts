834df73051cfabb48bea4455eddebebfe838e776
Georges-B. Michel <georges@reversense.com>
2022-01-28 04:45:45 +0100

    merge types

---
+++ src/arch/Types.ts
@@ -0,0 +1,39 @@
+
+export enum L {
+    PATH,
+    SIZE,
+    FD,
+    DFD,
+    FLAG,
+    ATTRMODE,
+    O_FLAGS,
+    VADDR,
+    MPROT,
+    OUTPUT_BUFFER,
+    PID,
+    ERR,
+    SIG,
+    XATTR_LIST,
+    F_,
+    MFD, // mapped fd
+    UID,
+    GID,
+    UTSNAME
+}
+
+export enum T {
+    INT32,
+    UINT32,
+    LONG,
+    ULONG,
+    SHORT,
+    USHORT,
+    FLOAT,
+    DOUBLE,
+    CHAR,
+    STRING,
+    CHAR_BUFFER,
+    POINTER32,
+    POINTER64,
+    STRUCT
+}
