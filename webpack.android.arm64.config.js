2289140f7f20dfa5cb8cabeb00914f9d46be7095
Georges-B. Michel <georges@reversense.com>
2022-01-28 04:45:27 +0100

    add webpack config

---
+++ webpack.android.arm64.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: './dist/index.js',
+    // devtool: 'inline-source-map',
+    target: 'node',
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'android-arm64-strace.min.js',
+        library: {
+            name: 'Interruptor',
+            type: 'commonjs'
+        }
+    },
+};
