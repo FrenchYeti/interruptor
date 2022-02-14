fb80a6760d2f60203fa6dd78c4409d6d7c759e2f
Georges-B. Michel <georges@reversense.com>
2022-02-14 02:03:36 +0100

    add follow thread feature

--- src/arch/LinuxArm64InterruptorFactory.ts
+++ src/arch/LinuxArm64InterruptorFactory.ts
@@ -10,6 +10,17 @@ import {AbstractInterruptorFactory} from "../common/AbstractInterruptorFactory";
 import {LinuxArm64InterruptorAgent, KAPI} from "./LinuxArm64InterruptorAgent";
 
 
+function deepCopy(pSrcObject:any): any {
+    const destObj = {};
+    for(let i in pSrcObject){
+        if(typeof pSrcObject[i] == 'object' && pSrcObject[i] !== null){
+            destObj[i] = deepCopy(pSrcObject[i]);
+        }else{
+            destObj[i] = pSrcObject[i];
+        }
+    }
+    return destObj;
+}
 
 export class LinuxArm64InterruptorFactory extends AbstractInterruptorFactory {
 
@@ -23,10 +34,43 @@ export class LinuxArm64InterruptorFactory extends AbstractInterruptorFactory {
 
     utils(){
         return {
-            toScanPattern: AbstractInterruptorFactory.toScanPattern
+            toScanPattern: AbstractInterruptorFactory.toScanPattern,
+            printBackTrace: AbstractInterruptorFactory.printBackTrace
         };
     }
 
+    static _followThread(pConfig:any){
+
+        console.error("Deploying pthread_create hook");
+        LinuxArm64InterruptorFactory.HOOKED_PTHREAD_ROUTINE = {};
+        Interceptor.attach( Module.findExportByName("libc.so","pthread_create"), {
+            onEnter: function(args){
+                let routine = args[2];
+
+                if(routine != null && !LinuxArm64InterruptorFactory.HOOKED_PTHREAD_ROUTINE.hasOwnProperty(routine)){
+                    LinuxArm64InterruptorFactory.HOOKED_PTHREAD_ROUTINE[routine+""]=true;
+                    console.log("["+Process.findModuleByAddress(this.context.pc).name+"] Hooking routine : "+routine+" "+JSON.stringify(LinuxArm64InterruptorFactory.HOOKED_PTHREAD_ROUTINE));
+                    Interceptor.attach( routine, {
+                        onEnter: function(a){
+                            const m = Process.findModuleByAddress(this.context.pc);
+                            console.log("------- [TID="+this.threadId+"]["+m.name+"]["+routine+"] Thread routine start -------");
+
+                            //console.log(JSON.stringify(self),self._pickThreadColor);
+                            const cfg = deepCopy(pConfig);
+                            cfg.output._tcolor = AbstractInterruptorFactory._pickThreadColor();
+                            const b = new LinuxArm64InterruptorAgent(cfg, this._followThread);
+                            LinuxArm64InterruptorFactory.AGENTS.push(b);
+                            b.start(this.threadId);
+                        },
+                        onLeave: function(a){
+                            console.log("------- [TID="+this.threadId+"]["+routine+"] Thread routine ended -------");
+
+                        }
+                    } )
+                }
+            }
+        });
+    }
     /**
      * To create a new Frida agent
      * @param pConfig
@@ -34,7 +78,9 @@ export class LinuxArm64InterruptorFactory extends AbstractInterruptorFactory {
     newAgentTracer(pConfig: any):InterruptorAgent {
         //const agent = new LinuxArm64InterruptorAgent(pConfig);
 
-        return new LinuxArm64InterruptorAgent(pConfig);
+        const agent = new LinuxArm64InterruptorAgent(pConfig, LinuxArm64InterruptorFactory._followThread);
+        //agent.buildScope();
+        return agent;
     }
 
 
