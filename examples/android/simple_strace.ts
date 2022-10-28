140be8167e2843bf923c5841d73235c797874501
Georges-B. Michel <georges@reversense.com>
2022-10-28 18:25:49 +0200

    Create new example

---
+++ examples/android/simple_strace.ts
@@ -0,0 +1,24 @@
+import target from '../../index.linux.arm64.js';
+
+const Interruptor = target.LinuxArm64({});
+
+Interruptor.newAgentTracer({
+    followThread: true,
+    scope: {
+        syscalls: {
+            exclude:  [/clock_gettime/]
+        },
+        modules: {
+            exclude: [/linker/]
+        }
+    },
+    onStart: function(){
+        console.log("Entering into lib")
+    }
+}).startOnLoad(/libNAME.so/,{ threshold:1 });
+
+
+
+
+
+
