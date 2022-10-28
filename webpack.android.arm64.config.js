2c21c22737daa2dbc83a31fb12a8fa9fc5b22909
Georges-B. Michel <georges@reversense.com>
2022-10-28 18:16:23 +0200

    Move to ESM

--- webpack.android.arm64.config.js
+++ webpack.android.arm64.config.js
@@ -1,17 +1,23 @@
-const path = require('path');
+import * as  path from 'path';
+import * as  url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const outputPath = path.resolve(__dirname, process.env.DXC_DIST ? 'dist':'.');
 
-module.exports = {
+const config = {
     entry: './index.linux.arm64.js',
     // devtool: 'inline-source-map',
-    target: 'node',
+    // target: 'node',
     mode: 'production',
     output: {
         path: outputPath,
         filename: 'android-arm64-strace.min.js',
-        library: {
-            //name: 'Interruptor',
-            type: 'commonjs'
+        library:{
+            type: 'module'
         }
     },
+    experiments: {
+        outputModule: true
+    }
 };
+
+export default config;
