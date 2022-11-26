e042cb6f6662c5f396efeea02ad3be9804ae9107
Georges-B. Michel <georges@reversense.com>
2022-11-27 00:20:50 +0100

    improve unit tests

--- test/InterruptorFactory.linux.arm64.ts
+++ test/InterruptorFactory.linux.arm64.ts
@@ -27,22 +27,24 @@ describe('[LINUX][AARCH64] InterruptorFactory tests', () => { // the tests conta
     describe('* Kernel API constants', () => {
 
         const factory = new LinuxArm64InterruptorFactory();
-        //const KAPI = factory.KAPI;
 
         it('Error Codes', () => {
-            expect(factory.KAPI).to.be.null;
+            /*
+                EPERM : [1,"Not super-user"],
+                ENOENT : [2,"No such file or directory"],
+                ESRCH : [3,"No such process"],
+                EINTR : [4,"Interrupted system call"],
+                EIO : [5,"I/O error"],
+             */
+            expect(factory.KAPI.ERR).to.be.not.null;
+            expect(Object.keys(factory.KAPI.ERR).length).to.be.greaterThan(120);
+            expect(factory.KAPI.ERR.EPERM).to.be.equal(1);
+            expect(factory.KAPI.ERR.ENOENT).to.be.equal(2);
+            expect(factory.KAPI.ERR.ESRCH).to.be.equal(3);
+            expect(factory.KAPI.ERR.EINTR).to.be.equal(4);
+            expect(factory.KAPI.ERR.EIO).to.be.equal(5);
         });
 
-        it('checking built-in utilities', () => {
-            const utils = factory.utils();
-
-            expect(Object.keys(utils).length).to.be.greaterThan(0);
-            expect(Object.values(utils).length).to.be.greaterThan(0);
-
-            expect(utils.toByteArray).to.be.a('function');
-            expect(utils.toScanPattern).to.be.a('function');
-            expect(utils.printBackTrace).to.be.a('function');
-        });
     });
 
 
